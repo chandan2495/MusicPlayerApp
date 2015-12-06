afb4a658a5bffedd0b7e7507c7579dd09b92596e
chandan2495 <chandan2495@gmail.com>
2015-12-06 22:25:43 +0530

    First App from code-tutsplus tutorial

---
+++ app/src/main/java/com/imsher/chandan/imsher/Song.java
@@ -0,0 +1,19 @@
+package com.imsher.chandan.imsher;
+
+/**
+ * Created by CHANDAN on 06-Dec-15.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
